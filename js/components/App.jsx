37ccf5eb720aa26d7fb644af258ea36318e26bac
Bartek <smolinb@gmail.com>
2019-02-27 01:43:19 +0100

    firebase added

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import Main from './Main.jsx';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import mysql from 'mysql';
+import firebase from '../firebase';
 
-class App extends Component { 
-    componentDidMount() {
-        const db = mysql.getInstance({
-            host: 'https://serwer1637184.home.pl/sql/',
-            port: 3306,
-            user: '21272731_kantor',
-            password: '4D^7q29tr5Gn',
-            database: '21272731_kantor'
-        })
-        
-        db.exec('select * from test')
-          .then(rows => rows.forEach(row => console.log(row)))
-          .catch(e => console.error(e))
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            users: ''
+        }
+    } 
+
+    componentWillMount() {
+        firebase.database().ref('/users/users/0/').on('value', (snapshot) => {
+            const userObj = snapshot.val();
+            
+            this.setState({
+                users: userObj.id
+            })
+          });
     }
-    
+
     render() {
         return (
-            <HashRouter>
-                <Switch>
-                    <Route exact path='/' component={Main} />
-                    <Route path='/login' component={Main} />
-                    <Route path='*' component={NotFound} />
-                </Switch>
-            </HashRouter>
+            // <HashRouter>
+            //     <Switch>
+            //         <Route exact path='/' component={Main} />
+            //         <Route path='/login' component={Main} />
+            //         <Route path='*' component={NotFound} />
+            //     </Switch>
+            // </HashRouter>
+            <h1>{this.state.users}</h1>
         )
     }
 }
---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyC40Z2y0aDxnktyEU5FIfq4gY_9c9dLewI",
+    authDomain: "gielda-856fa.firebaseapp.com",
+    databaseURL: "https://gielda-856fa.firebaseio.com",
+    projectId: "gielda-856fa",
+    storageBucket: "gielda-856fa.appspot.com",
+    messagingSenderId: "740732197790"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
